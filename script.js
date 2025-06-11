0541d641305bf081ef45458b445a20e3d47f874a
Adem Ben Zineb <adem.benzineb.dev@gmail.com>
2025-06-11 17:10:46 +0200

    Nothing just trying to fix the js problem

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-function grids16(){
-    const cont = document.querySelector("#container");
-    for (let i = 0 ; i < 50 ; i++){
+
+const containerSize = 1000; // Size of the container in pixels
+const cont = document.querySelector("#container");
+const gridSize = 50; // Number of grid items
+cont.style.width = `${containerSize}px`;
+cont.style.height = `${containerSize}px`;
+
+function CreateGrid(){
+    const gridnum = (containerSize / gridSize)**2; // Calculate the size of each grid item
+    for (let i = 0 ; i < gridnum ; i++){
         let div = document.createElement("div");
+        div.style.width = gridSize + "px";
+        div.style.height = gridSize + "px";
         div.classList.add("grid");
         cont.appendChild(div)
-
+        //document.addEventListener("mouseover", colorChange )
     }
-}
-function colorChange(){
-    let grid = document.querySelector(".grids");
 }
